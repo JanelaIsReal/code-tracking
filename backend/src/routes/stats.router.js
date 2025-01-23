b204f14779948100f1da0ce995d682c91e0704fc
Henrique Moura <vamoscorrerpodcast@gmail.com>
2025-01-23 21:15:58 +0000

    DevTrack: Files: song.controller.js, user.routes.js, user.controller.js, stats.router.js, stats.controller.js. [main]  | Changes in: song.controller.js; user.routes.js; user.controller.js (modified router; added import, try, totalSongs, totalUsers, totalAlbums, next; removed res); stats.router.js; stats.controller.js

--- backend/src/routes/stats.router.js
+++ backend/src/routes/stats.router.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import {Song} from "../models/songs.model.js";
+import {User} from "../models/user.model.js";
+import {Album} from "../models/album.model.js";
+
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Stats route with GET method");  
-}); 
+router.get("/", async (req, res) =>{
+    try {
+        const totalSongs = await Song.countDocuments();
+        const totalUsers = await User.countDocuments();
+        const totalAlbums = await Album.countDocuments();
+
+    } catch (error) {
+        next(error);
+    }
+} ); 
 
 export default router;
