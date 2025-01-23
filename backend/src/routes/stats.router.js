138a23f7db9358d832aa870cb302ffe1b3b24559
Henrique Moura <vamoscorrerpodcast@gmail.com>
2025-01-23 21:19:23 +0000

    a

--- backend/src/routes/stats.router.js
+++ backend/src/routes/stats.router.js
@@ -8,9 +8,32 @@ const router = Router();
 
 router.get("/", async (req, res) =>{
     try {
-        const totalSongs = await Song.countDocuments();
-        const totalUsers = await User.countDocuments();
-        const totalAlbums = await Album.countDocuments();
+        // const totalSongs = await Song.countDocuments();
+        // const totalUsers = await User.countDocuments();
+        // const totalAlbums = await Album.countDocuments();
+        
+        const [totalSongs, totalUsers, totalAlbums, uniqueArtists] = await Promise.all([
+            Song.countDocuments(),
+            User.countDocuments(),
+            Album.countDocuments(),
+
+            Song.aggregate([
+                {
+                    $unionWith:{
+                        coll:"albums",
+                        pipeline:[]
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$artist",
+                    }
+                },
+                {
+                    $count: "count"
+                }
+            ])
+        ]);
 
     } catch (error) {
         next(error);
